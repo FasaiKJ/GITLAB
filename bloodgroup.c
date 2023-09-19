137f04a40b54bfa4b5edc774d4a4a9f72b0f1a3a
FasaiKJ <fasai.ku18@gmail.com>
2023-09-19 23:17:30 +0700

    Modifiled bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -39,10 +39,10 @@ void main()
     else
     {
       printf("%s, O. Nice! You can give blood to all: O, A, B, AB\n", Name);
-      printf("  So sad! You can receive blood only from: O\n");
+      printf("  But sad! You can receive blood only from: O\n");
     }
     printf("\nContinue (YES for Yes)? ");
     scanf("%s", answer);
   } while (strcmp(answer, "YES") == 0);
-  printf("Goodbye\n");
+  printf("Bye Bye\n");
 }
