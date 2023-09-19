e5e73151efcd70a49bc8189c1f31f52a2a369af8
FasaiKJ <fasai.ku18@gmail.com>
2023-09-19 22:28:31 +0700

    Modified bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 void main()
-{ char Name[40], G[3], answer[10];
+{ char Name[50], G[5], answer[10];
   int i;
   do
   {
-    printf("What is your Name? ");
+    printf("What is your name? ");
     scanf("%s", Name);
     do
     {
-      printf("What is your blood type [A, B, AB, or O]? ");
+      printf("What is your blood group [A, B, AB, or O]? ");
       scanf("%s", G);
 
       if (strcmp(G, "A") !=0  &&
